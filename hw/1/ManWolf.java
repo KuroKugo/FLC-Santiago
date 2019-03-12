cf4b9c710b15d1a093f00e69f0bdf7f78e6f3051
Christian Santiago <asuna@Christians-MacBook-Pro.local>
2019-03-11 23:17:49 -0400

    Included File Descriptions

---
+++ hw/1/ManWolf.java
@@ -0,0 +1,11 @@
+  /**
+  *  file: ManWolf.java
+  *  author: Christian Santiago
+  *  course: CMPT 440
+  *  assignemnt: homework 1
+  *  due date: March 12, 2019
+  *  version 0.1
+  *
+  *  This file contains the declaration of the abstract
+  *  class for the ManWolf problem
+  */
---
+++ hw/1/driverDFA.java
@@ -0,0 +1,11 @@
+  /**
+  *  file: driverDFA.java
+  *  author: Christian Santiago
+  *  course: CMPT 440
+  *  assignemnt: homework 1
+  *  due date: March 12, 2019
+  *  version 0.1
+  *
+  *  This file contains the implementation of the ManWolf
+  *  class
+  */
