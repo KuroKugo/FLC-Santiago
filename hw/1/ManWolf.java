e3b6dead07c240db2259f6e23dc0acd27bdf8eef
Christian Santiago <asuna@148-100-188-212.FoxNet.marist.edu>
2019-03-12 18:00:10 -0400

    Moved static var to abstact class

--- hw/1/ManWolf.java
+++ hw/1/ManWolf.java
@@ -25,6 +25,31 @@
     // protected final static State STARTSTATE = State.Q0;
     // protected final static State ACCEPTSTATE = State.Q9;
     // protected final static State ERRORSTATE = State.Q10;
+
+    protected static final int q0 = 0;
+    protected static final int q1 = 1;
+    protected static final int q2 = 2;
+    protected static final int q3 = 3;
+    protected static final int q4 = 4;
+    protected static final int q5 = 5;
+    protected static final int q6 = 6;
+    protected static final int q7 = 7;
+    protected static final int q8 = 8;
+    protected static final int q9 = 9;
+    protected static final int q10 = 10;
+
+    static protected int[][] delta = 
+    { {q10, q1, q10, q10},
+     {q10, q0, q10, q2}, 
+     {q3, q10, q5, q1}, 
+     {q2, q4, q10, q10},
+     {q10, q3, q7, q10},
+     {q10, q6, q2, q10},
+     {q7, q5, q10, q10},
+     {q6, q10, q4, q8},
+     {q10, q9, q10, q7},
+     {q10, q8, q10, q10},
+     {q10, q10, q10, q10} };
     
 
     abstract void reset();
@@ -32,7 +57,7 @@
 
     abstract void process(String s);
 
-    
+
     abstract boolean accepted();
 
   }
--- hw/1/driverDFA.java
+++ hw/1/driverDFA.java
@@ -13,31 +13,6 @@
   public class driverDFA extends ManWolf {
     private int state;
 
-    private static final int q0 = 0;
-    private static final int q1 = 1;
-    private static final int q2 = 2;
-    private static final int q3 = 3;
-    private static final int q4 = 4;
-    private static final int q5 = 5;
-    private static final int q6 = 6;
-    private static final int q7 = 7;
-    private static final int q8 = 8;
-    private static final int q9 = 9;
-    private static final int q10 = 10;
-
-    static private int[][] delta = 
-    { {q10, q1, q10, q10},
-     {q10, q0, q10, q2}, 
-     {q3, q10, q5, q1}, 
-     {q2, q4, q10, q10},
-     {q10, q3, q7, q10},
-     {q10, q6, q2, q10},
-     {q7, q5, q10, q10},
-     {q6, q10, q4, q8},
-     {q10, q9, q10, q7},
-     {q10, q8, q10, q10},
-     {q10, q10, q10, q10} };
-
     public void reset() {
       // state = STARTSTATE;
       state = q0;
