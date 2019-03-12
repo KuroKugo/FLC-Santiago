81f409641cb256caf3c891d1d4c5ebc9e3600349
Christian Santiago <asuna@Christians-MacBook-Pro.local>
2019-03-12 13:08:41 -0400

    Created Class using table approach

--- hw/1/ManWolf.java
+++ hw/1/ManWolf.java
@@ -14,19 +14,25 @@
    *  ManWolf
    *  
    *  This class implements the all the states in the ManWolf
-   *  problem and sets up the declaration for the transition funtion
+   *  problem and sets up the declaration for the required funtions
    */
   public abstract class ManWolf {
-    protected final static enum State { Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, 
-                            Q8, Q9, Q10, Q11 }
+    // protected final static enum State { Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7, 
+    //                         Q8, Q9, Q10 }
     
-    protected final static alphabet = ['n', 'w', 'g', 'c'];
+    //protected final static alphabet = ['n', 'w', 'g', 'c'];
     
-    protected startState = State.Q0;
-    protected finalState = State.Q10;
+    // protected final static State STARTSTATE = State.Q0;
+    // protected final static State ACCEPTSTATE = State.Q9;
+    // protected final static State ERRORSTATE = State.Q10;
+    
 
     abstract void reset();
-    abstract void process();
+
+
+    abstract void process(String s);
+
+    
     abstract boolean accepted();
 
   }
--- hw/1/driverDFA.java
+++ hw/1/driverDFA.java
@@ -9,3 +9,82 @@
   *  This file contains the implementation of the ManWolf
   *  class
   */
+
+  public class driverDFA extends ManWolf {
+    private int state;
+
+    private static final int q0 = 0;
+    private static final int q1 = 1;
+    private static final int q2 = 2;
+    private static final int q3 = 3;
+    private static final int q4 = 4;
+    private static final int q5 = 5;
+    private static final int q6 = 6;
+    private static final int q7 = 7;
+    private static final int q8 = 8;
+    private static final int q9 = 9;
+    private static final int q10 = 10;
+
+    static private int[][] delta = 
+    { {q10, q1, q10, q10},
+     {q10, q0, q10, q2}, 
+     {q3, q10, q5, q1}, 
+     {q2, q4, q10, q10},
+     {q10, q3, q7, q10},
+     {q10, q6, q2, q10},
+     {q7, q5, q10, q10},
+     {q6, q10, q4, q8},
+     {q10, q9, q10, q7},
+     {q10, q8, q10, q10},
+     {q10, q10, q10, q10} };
+
+    public void reset() {
+      // state = STARTSTATE;
+      state = q0;
+    }
+
+    public boolean accepted() {
+      return state == q9;
+    }
+
+
+    public void process(String s) {
+      for (int i = 0; i < s.length(); i++) {
+         char c = s.charAt(i);
+         try {
+           if (c == 'w') {
+            state = delta[state][0];
+           }
+           if (c == 'g') {
+            state = delta[state][1];
+           }
+           if (c == 'c') {
+            state = delta[state][2];
+           }
+           if (c == 'n') {
+            state = delta[state][3];
+           }
+         }
+         catch (ArrayIndexOutOfBoundsException ex) {
+          //  state = ERRORSTATE;
+          state = q10;
+           } 
+      }
+    }
+
+    public static void main(String[] args) {
+
+      driverDFA d = new driverDFA();
+       
+       String s = args[0];
+        d.reset();
+        d.process(s);
+        if (d.accepted()) {
+        System.out.printf("That is a solution.\n");
+        }
+        else {
+          System.out.printf("That is not a solution.\n");
+        }
+
+    }
+  }
