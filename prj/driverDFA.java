102dde6de75bcf307505b86e995f93c6eac8f148
Christian Santiago <asuna@148-100-143-136.FoxNet.marist.edu>
2019-05-07 23:56:33 -0400

    Refined transition, added interface

--- prj/driverDFA.java
+++ prj/driverDFA.java
@@ -1,22 +1,37 @@
 import java.util.ArrayList;
-
+import java.util.Arrays;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class driverDFA extends wordFilter {
+    private int errorState;
     private int state;
     private int transState;
+    private int alphabet;
     private Pattern regex = Pattern.compile("[a-z]*");
     private int a = (int) 'a';
     int[][] deltaTrans;
-    private ArrayList<Integer> accept = new ArrayList<Integer>(Arrays.asList(q3, q8, q13, q17, q21, q29, q33));
+    // private ArrayList<Integer> accept = new ArrayList<Integer>(Arrays.asList(q3, q8, q13, q17, q21, q29, q33));
     private ArrayList<Integer> acceptedStates = new ArrayList<>();
+    private ArrayList<String> wordList = new ArrayList<>();
 
     public void reset() {
       // state = STARTSTATE;
-      state = q0;
-
+      // state = q0;
+      state = 0;
     }
 
     public boolean accepted() {
@@ -30,7 +45,7 @@ public class driverDFA extends wordFilter {
       String str = s.toLowerCase();
       Matcher m = regex.matcher(str);
       boolean match = m.matches();
-      System.out.println(match);
+      // System.out.println(match);
       if (match)
       {
         for (int i = 0; i < str.length(); i++) {
@@ -42,14 +57,14 @@ public class driverDFA extends wordFilter {
           }
           catch (ArrayIndexOutOfBoundsException ex) {
             //  state = ERRORSTATE;
-            state = q34;
+            state = errorState;
             }
             
         }
       }
       else
       {
-        state = q34;
+        state = errorState;
       }
     }
 
@@ -57,42 +72,49 @@ public class driverDFA extends wordFilter {
       int transitions = 0;
       String[] words;
 
-      words = allowedWords;
-      for (String word : words) {
+      // words = allowedWords;
+      // for (String word : words) {
+      //   transitions += word.length();
+      // }
+
+      // for (String word : allowedWords) {
+      //   wordList.add(word);
+      // }
+      for (String word : wordList) {
         transitions += word.length();
       }
       
       deltaTrans = new int[transitions + 26][26];
-      int errorState = deltaTrans.length - 1;
-      String testWord = words[0];
+      errorState = deltaTrans.length - 1;
+      // String testWord = wordList.get(0);
 
-      System.out.println(" " + transitions);
+      // System.out.println(" " + transitions);
 
       for (int i = 0; i < 26; i ++) {
         deltaTrans[0][i] = i + 1;
-        System.out.println(" Delta: [0][" + i + "] = " + (i + 1));
+        // System.out.println(" Delta: [0][" + i + "] = " + (i + 1));
       }
       for (int i = 1; i < deltaTrans.length; i ++) {
         for (int j = 0; j < 26; j ++) {
           deltaTrans[i][j] =  errorState;
         }
-        System.out.println(" Transition at index: deltaTrans[" + i + "] has been filled with " + errorState);
+        // System.out.println(" Transition at index: deltaTrans[" + i + "] has been filled with " + errorState);
       }
 
-      for (int d = 0; d < allowedWords.length; d ++) {
-        for (int i = 0; i < allowedWords[d].length(); i++) {
-          char c = allowedWords[d].charAt(i);
+      for (int d = 0; d < wordList.size(); d ++) {
+        for (int i = 0; i < wordList.get(d).length(); i++) {
+          char c = wordList.get(d).charAt(i);
           int letter = (int) c;
           int nextState = letter - a;
           try {
-            System.out.println(allowedWords[d] + " current Delta: [" + transState + "][" + nextState + "]");
+            // System.out.println(wordList.get(d) + " current Delta: [" + transState + "][" + nextState + "]");
             if (i == 0) {
               transState = deltaTrans[0][nextState];
             } else {
                 if (deltaTrans[transState][nextState] == errorState) {
                   deltaTrans[transState][nextState] = 26 + i + d;
-                  System.out.println("Calc next State " + deltaTrans[transState][nextState]);
-                  if (i + 1 == allowedWords[d].length()) {
+                  // System.out.println("Calc next State " + deltaTrans[transState][nextState]);
+                  if (i + 1 == wordList.get(d).length()) {
                     acceptedStates.add(deltaTrans[transState][nextState]);
                   }
                 }
@@ -102,24 +124,114 @@ public class driverDFA extends wordFilter {
             //  state = ERRORSTATE;
             transState = errorState;
           }
-          System.out.println(allowedWords[d] + " next state: " + transState);
+          // System.out.println(wordList.get(d) + " next state: " + transState);
         }
       }
     }
 
+    public void addWord(String word) {
+		
+      BufferedWriter writer;
+      
+      try {
+        writer = new BufferedWriter(new FileWriter("./censor.txt", true));
+        if (!wordList.contains(word)) {
+          writer.write(word);
+          writer.newLine();
+          System.out.printf("The word " + word + " has been added to the filter.\n");
+        }
+        writer.flush();
+        
+        writer.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }	
+    }
+
+    public void loadWordList() {
+      BufferedReader reader;
+      try {
+        reader = new BufferedReader(new FileReader("./censor.txt"));
+        
+        String line = reader.readLine();
+        
+        while(line != null) {
+          System.out.println(line);
+          wordList.add(line);
+          line = reader.readLine();
+        }
+        
+        reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public void printWordList () {
+      for (String word : wordList) {
+        // System.out.println("  " + word + "  ");
+      }
+    }
+
     public static void main(String[] args) {
-        String s = args[0];
+      boolean inputMode = true;
+      boolean filterMode = false;
+      String s = "";
       driverDFA d = new driverDFA();
+      System.out.println("-----------------------------------------");
+
+      System.out.println("Welcome to the Profanity Filter");
+      System.out.println("Current words in the Profanity Filter");
+
+      d.loadWordList();
+      System.out.println("-----------------------------------------");
+      d.printWordList();
+
+      Scanner scanner = new Scanner(System.in);
+      try {
+          while (inputMode) {
+              System.out.print("Add words by entering a word");
+              System.out.println("or exit input mode by entering \"exit\"");
+              String line = scanner.next();
+              if (line.equals("exit")) {
+                inputMode = false;
+                filterMode = true;
+              } else {
+                d.addWord(line);
+              }
+          }
+      } catch(IllegalStateException | NoSuchElementException e) {
+          // System.in has been closed
+          System.out.println("System.in was closed; exiting");
+      }
+
+      System.out.println("-----------------------------------------");
+      System.out.println("Current words in the Profanity Filter");
+      d.loadWordList();
+      System.out.println("To test the filter enter a word");
+      System.out.println("-----------------------------------------");
       d.createTransition();
 
-      d.reset();
-      d.process(s);
-      if (d.accepted()) {
-      System.out.printf("You said a bad word\n");
-      }
-      else {
-        System.out.printf("That is not a bad word.\n");
+      while (filterMode) {
+        System.out.println("Enter a word and I will tell you if it is a bad word.");
+        System.out.println("Or exit by entering \"exit\"");
+        String line = scanner.next();
+        s = line;
+
+        d.reset();
+        d.process(s);
+        if (d.accepted()) {
+        System.out.println("You said a bad word\n");
+        }
+        else {
+          System.out.println("That is not a bad word.\n");
+        }
+        if (s.equals("exit")) {
+          filterMode = false;
+        }
       }
 
+      System.out.println("-----------------------------------------");
     }
   }
